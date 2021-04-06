d7a7ae297455981d473fc502b720238e12db0c59
Rashmi <rashmi.gitu@gmail.com>
2021-04-06 09:18:46 +0530

    added HelloWorld program

---
+++ src/io/core/HelloWorld.java
@@ -0,0 +1,12 @@
+package io.core;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello Jenkins First file." + i);
+		}
+
+	}
+
+}
